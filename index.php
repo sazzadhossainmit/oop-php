62ef1e9be92cd2e56e3c2f963a4c759a6dcc5578
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-06 21:56:34 +0600

    practice method and object, calculating two numbers

--- index.php
+++ index.php
@@ -3,27 +3,80 @@
 ?>
 
 
+<form action="" method="POST">
+  <table>
+    <tr>
+      <td>Enter the first number</td>
+      <td><input type="number" name="num1"></td>
+    </tr>
+    <tr>
+      <td>Enter the second number</td>
+      <td><input type="number" name="num2" ></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td><input type="submit" name="calculation" value="Calculate"></td>
+    </tr>
+  </table>
+</form>
+
 <?php
+	//form validation
+  if(isset($_POST['calculation'])){
+    $numOne = $_POST['num1'];
+    $numTwo = $_POST['num2'];
 
-  class Person {
-    public $name;
-    public $age;
+    if(empty($numOne) or empty($numTwo)){
+      echo "<span style='color:red;' > Please fill out these fields </span><br>";
+    } else{
 
-    public function personName(){
-      echo "Person's name is: ". $this->name. "<br>";
+      echo "<span style='font-style:italic;'>Calculation of the number ".$numOne. " & ".$numTwo. "</span><br>";
+			
+			// object
+      $calc = new Compute;
+      $calc->add($numOne, $numTwo);
+      $calc->sub($numOne, $numTwo);
+      $calc->multi($numOne, $numTwo);
+      $calc->div($numOne, $numTwo);
     }
 
-    public function personAge($value){
-      echo "Person's age is: ". $this->age=$value;
+  }
+?>
+
+<?php
+	// class
+  class Compute{
+    
+    public function add($a, $b){
+      $a = (float)$a;
+      $b = (float)$b;
+
+      echo "Summation = ". ($a+$b)."<br>";
+    }
+
+    public function sub($a, $b){
+      $a = (float)$a;
+      $b = (float)$b;
+
+      echo "Substraction = ". ($a-$b)."<br>";
+    }
+
+    public function multi($a, $b){
+      $a = (float)$a;
+      $b = (float)$b;
+
+      echo "Multiplication = ". ($a*$b)."<br>";
+    }
+
+    public function div($a, $b){
+      $a = (float)$a;
+      $b = (float)$b;
+
+      echo "Division = ". ($a/$b);
     }
   }
 
-  $personOne = new Person;
-  $personOne->name = "Sazzad Hossain";
-  $personOne->personName();
-  $personOne->personAge("23");
-
-?> 
+?>
 
 
 
