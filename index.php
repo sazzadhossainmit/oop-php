e0e382e3d8bc798afbe39c7f056ca7f28f46807d
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-06 22:52:01 +0600

    Preventing from Extending & Overriding 'final' keyword

--- index.php
+++ index.php
@@ -4,43 +4,27 @@
 
 
 <?php
+	final class A {
+}
 
-  class UserData{
-    public $user;
-    public $userId;
+class B extends A { }   // ERROR: Cannot extend final class
+?>
 
-    public function __construct($usser, $usserId){
-      $this->user = $usser;
-      $this->userId = $usserId;
-
+<?php
+	class ParentClass {
+    final public function show() {
+        echo "Cannot override this!";
     }
+}
 
-    public function display(){
-      echo "Username is {$this->user} and user ID is {$this->userId}<br>";
+class ChildClass extends ParentClass {
+    public function show() {
+        echo "Trying to override";  //  ERROR
     }
-
-  }
-
-  class Admin extends UserData{
-    public $level;
-
-    public function display(){
-      echo "Username is {$this->user} and user ID is {$this->userId} and user level is {$this->level}.<br>";
-    }
-
-  }
-
-$ur = "Sazzad";
-$id = 20;
-  $usr = new UserData($ur, $id);
-
-$aname = "Admin";
-$aid = 9;
-  $ad = new Admin($aname, $aid);
-  $ad->level = "Administrator";
-  $ad->display();
+}
 
 ?>
+
 
 
 
