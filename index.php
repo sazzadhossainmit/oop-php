66ed33e70f99235753e8f8a0ec5be3a720c9cbae
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-06 21:52:49 +0600

    Class, Property, Method and Object

--- index.php
+++ index.php
@@ -3,7 +3,27 @@
 ?>
 
 
+<?php
 
+  class Person {
+    public $name;
+    public $age;
+
+    public function personName(){
+      echo "Person's name is: ". $this->name. "<br>";
+    }
+
+    public function personAge($value){
+      echo "Person's age is: ". $this->age=$value;
+    }
+  }
+
+  $personOne = new Person;
+  $personOne->name = "Sazzad Hossain";
+  $personOne->personName();
+  $personOne->personAge("23");
+
+?> 
 
 
 
