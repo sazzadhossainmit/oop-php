db01e4d562d8234056b1b9972516e0c08bfe33f7
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-09 21:51:38 +0600

    Object iteration: using foreach on an object to loop through it's properties

--- index.php
+++ index.php
@@ -4,50 +4,32 @@
 
 
 <?php
-// array type 
-	class Calculation{
-    public $a;
-    public function getValue(array $x){
-      foreach($x as $value){
-        echo $value[0];
-        echo " : ";
-        echo $value[1]*$value[2]. "<br>";
+  class Person {
+    public $name = "Sazzad";
+    public $age = "20";
+    public $skill = "Blogging";
+
+    private $email = "sazzadhossain@gmail.com";
+    protected $password = "1234";
+
+    public function iteratorInner(){
+      echo "Inside class <br>";
+      foreach($this as $key => $value){
+        echo "<pre>";
+        echo "$key => $value";
+        echo "</pre>";
       }
     }
   }
   
-  $num = array(
-    array('Number one',10,20),
-    array('Number two',5,15)
-  );
-  $calc = new Calculation();
-  echo $calc->getValue($num);  
-?>
-
-
-<?php
-  // object type
-  class Java{
-    public $fromPhp;
-    function __construct(Php $value){
-      $this->fromPhp = $value;
-      $this->fromPhp->framework();
-      $this->fromPhp->cms();
-    }
+  $obj = new Person();
+  foreach($obj as $key => $value){
+    echo "<pre>";
+    echo "$key => $value";
+    echo "</pre>";
   }
 
-  class Php{
-    public function framework(){
-      echo "Laravel is a PHP framework <br>";
-      return $this;
-    }
-    public function cms(){
-      echo "WordPress is a CMS";
-      return $this;
-    }
-  }
-  $obj = new Php();
-  new Java($obj);
+  $obj->iteratorInner();
 ?>
 
 
