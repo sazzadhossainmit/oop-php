9092ae1e176bc17b4a53f981f3f3995a288ed294
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-09 21:44:48 +0600

    Type hinting: telling PHP what type of data a function or method must receive or return

--- index.php
+++ index.php
@@ -4,25 +4,50 @@
 
 
 <?php
-  class Calculation{
-    public $a = 0;
-    public $b = 0;
-    public $result;
-
-    public function getValue($x, $y){
-      $this->a = $x;
-      $this->b = $y;
-      return $this;
-    }
-
-    public function getResult(){
-      $this->result = $this->a * $this->b;
-      return $this->result;
+// array type 
+	class Calculation{
+    public $a;
+    public function getValue(array $x){
+      foreach($x as $value){
+        echo $value[0];
+        echo " : ";
+        echo $value[1]*$value[2]. "<br>";
+      }
     }
   }
   
+  $num = array(
+    array('Number one',10,20),
+    array('Number two',5,15)
+  );
   $calc = new Calculation();
-  echo "Result is ". $calc->getValue(3,5)->getResult();
+  echo $calc->getValue($num);  
+?>
+
+
+<?php
+  // object type
+  class Java{
+    public $fromPhp;
+    function __construct(Php $value){
+      $this->fromPhp = $value;
+      $this->fromPhp->framework();
+      $this->fromPhp->cms();
+    }
+  }
+
+  class Php{
+    public function framework(){
+      echo "Laravel is a PHP framework <br>";
+      return $this;
+    }
+    public function cms(){
+      echo "WordPress is a CMS";
+      return $this;
+    }
+  }
+  $obj = new Php();
+  new Java($obj);
 ?>
 
 
