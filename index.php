8e9ec0facbe954ba3a4158d904fe63afb74b55b7
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-09 22:06:46 +0600

    __CLASS__ and get_class()

--- index.php
+++ index.php
@@ -5,28 +5,30 @@
 
 <?php
 
-  trait Php{
-    public function phpCoder(){
-      return "I love PHP <br>";
+	abstract class Person{
+    public function details(){
+      echo "Parent/Base class constant and class name: ".__CLASS__."<br>";
+      echo "Parent/Base class function and class name: ".get_class($this)."<br>";
     }
   }
+  
+  class ChildPerson extends Person{
+    public function childDetails(){
+      echo "Child class constant and class name: ".__CLASS__."<br>";
+      echo "Child class function and class name: ".get_class($this)."<br>";
+    }
 
-  trait Java{
-    public function javaCoder() {
-      return "I love JAVA <br>";
+    public function ourMethod(){
+      parent::details();
     }
   }
-
-  trait phpJava{
-    use Php, Java;
-  }
-  class coderOne{
-    use phpJava;
-  }
-
-  $obj1 = new coderOne();
-  echo $obj1->phpCoder();
-  echo $obj1->javaCoder();
+  
+  $obj = new ChildPerson();
+  $obj->details();
+  echo "<hr>";
+  $obj->childDetails();
+  echo "<hr>";
+  $obj->ourMethod();
 ?>
 
 
