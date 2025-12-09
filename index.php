50c0dd2e3745598e516383df6108163978eeca50
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-09 20:40:34 +0600

    (Interface  interface → implements)

--- index.php
+++ index.php
@@ -4,31 +4,57 @@
 
 
 <?php
+// overriden multiple interfaces with multiple classes implemented
+  interface School{
+    public function mySchool();
+  }
+  interface College{
+    public function myCollege();
+  }
+  interface Varsity{
+    public function myVarsity();
+  }
 
-  class Animal{
-    public function sound(){
-      echo "Animal makes noise.";
+  class Teacher implements School, College, Varsity{
+
+    public function __construct(){
+      $this->mySchool();
+      $this->myCollege();
+      $this->myVarsity();
+    }
+
+    public function mySchool(){
+      echo "I am a school teacher. <br>";
+    }
+    public function myCollege(){
+      echo "I am a college teacher. <br>";
+    }
+    public function myVarsity(){
+      echo "I am a varsity teacher. <br>";
     }
   }
 
-  class Dog extends Animal{
-    public function sound(){
-      echo "Dog barks.";
+  class Student implements School, College, Varsity{
+
+    public function __construct(){
+      $this->mySchool();
+      $this->myCollege();
+      $this->myVarsity();
+    }
+
+    public function mySchool(){
+      echo "I am a school student. <br>";
+    }
+    public function myCollege(){
+      echo "I am a college student. <br>";
+    }
+    public function myVarsity(){
+      echo "I am a varsity student. <br>";
     }
   }
 
-  class Cat extends Animal{
-    public function sound(){
-      echo "Cat meows.";
-    }
-  }
-
-  $doggy = new Dog();
-  $doggy->sound();
-  echo "<br>";
-  $catty = new Cat();
-  $catty->sound();
-
+  $teacher = new Teacher();
+  $student = new Student();
 ?> 
 
 
