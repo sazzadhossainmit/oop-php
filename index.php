5bcc6a3259a72a32fd984857c16e5eaaddffb0eb
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-16 19:47:23 +0600

    Standard PHP Library - SPL

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php
+
+use Dom\CharacterData;
+
 include('inc/header.php');
 ?>
 
 
 <?php
-/*
+
   spl_autoload_register(function($class_name){
     include "classes/".$class_name.".php";
   });
 
-  */
 
-  // for($i=0, $i<10, $i++){};
-
-  $arr = array("PHP","HTML","CSS","JavaScript");
-
-  $coding = new ArrayObject($arr);
-  $coding->append("JAVA");
-  $iterator = $coding->getIterator();
-  while($iterator->valid()){
-    echo $iterator->current()."<br>";
-    $iterator->next();
+  $arr = array("PHP","HTML","CSS","JavaScript","JAVA");
+  $ai = new ArrayIterator($arr);
+  // $limit = new LimitIterator($ai,0,2);
+  $ci = new CachingIterator($ai);
+  foreach($ci as $value){
+    echo $value;
+    if($ci->hasNext()){
+      echo ", ";
+    }
   }
 
 ?>
