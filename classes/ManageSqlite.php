9fdc62db2bd65018e3eb1e61077d39fa28c98cc6
Sazzad Hossain <sazzadhossainmit@gmail.com>
2025-12-18 18:28:40 +0600

    Factory design pattern

---
+++ classes/ManageSqlite.php
@@ -0,0 +1,19 @@
+<?php
+  class ManageSqlite{
+    public function setHost($host){
+      // your code will go here...
+    }
+    public function setDB($db){
+      // your code will go here...
+    }
+    public function setUser($user){
+      // your code will go here...
+    }
+    public function setPass($pass){
+      // your code will go here...
+    }
+    public function connect(){
+      // your connection code will go here...
+    }
+  }
+?>
--- index.php
+++ index.php
@@ -7,21 +7,9 @@
     include "classes/".$class_name.".php";
   });
 
-  $user = new User();
-  $msg = $user->getMsg();
-
-  switch($msg){
-    case 'email':
-      $objmsg = new SendEmail();
-      break;
-    case 'sms':
-      $objmsg = new SendSms();
-      break;
-    case 'fax': 
-      $objmsg = new SendFax();
-      break;
-  }
-  $objmsg->notification();
+  $db = new Database();
+  $db->setDriver("sqlite");
+  $db->connect("host","user","dbname","pass");
 ?>
 
 
