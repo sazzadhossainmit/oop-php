4bf1071fa5b97dcf97e53db3be91286aded26af6
sazzad-hossain <sazzadhossainmit@gmail.com>
2025-12-28 22:16:03 +0600

    PDO - PHP data object

--- index.php
+++ index.php
@@ -3,30 +3,21 @@
 ?>
 
 <?php
-  $db = new mysqli("localhost", "root", "", "test");
-  if(mysqli_connect_errno()){
-    echo "Connection failed...";
-    exit();
+  $dsn = "mysql:dbname=userdata;host=localhost;";
+  $user = "root";
+  $pass = "";
+
+  try{
+    $pdo = new PDO($dsn, $user, $pass);
+  }catch(PDOException $e){
+    echo "connection failed...".$e->getMessage();
   }
 
-$sql = "SELECT image FROM images WHERE id=?";
-$stmt = $db->prepare($sql);
-$stmt->bind_param("i", $id);
-$id = 1;
-$stmt->execute();
-$stmt->bind_result($image);
-$stmt->fetch();
-header("content-type: png");
-echo '<img style="max-width:300px;" src="data:image/png;base64, '.base64_encode($image).'"/>';
-
-  /*
-  $sql = "INSERT INTO images(image) VALUES(?)";
-  $stmt = $db->prepare($sql);
-  $stmt->bind_param("b", $image);
-  $image = file_get_contents("sazzad.png");
-  $stmt->send_long_data(0, $image);
-  $stmt->execute();
-  */
+  $sql = "SELECT * FROM tbl_user";
+  $results = $pdo->query($sql);
+  foreach($results as $result){
+    echo $result['skill']."<br>";
+  }
 ?>
 
 
