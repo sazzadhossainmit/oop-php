ac512712c3593d1df635ecc89071fef51b1627d5
Sazzad Hossain <sazzadhossainmit@gmail.com>
2026-02-15 11:29:21 +0600

    Prepared Statements with PDO

--- index.php
+++ index.php
@@ -13,11 +13,25 @@
     echo "connection failed...".$e->getMessage();
   }
 
-  $sql = "SELECT * FROM tbl_user";
-  $results = $pdo->query($sql);
-  foreach($results as $result){
-    echo $result['skill']."<br>";
-  }
+  $name = "Arif Khan";
+  $email = "arifkhan@gmail.com";
+  $skill = "JAVASCRIPT";
+  $age = 32;
+
+  /*
+  $sql = " INSERT INTO tbl_user(name, email, skill, age) VALUES(?, ?, ?, ?) ";
+  $stmt = $pdo->prepare($sql);
+  $arr = array($name, $email, $skill, $age);
+  $stmt->execute($arr);
+  */
+
+  $sql = " INSERT INTO tbl_user(name, email, skill, age) VALUES(:name, :email, :skill, :age) ";
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindParam(':name', $name);
+  $stmt->bindParam(':email', $email);
+  $stmt->bindParam(':skill', $skill);
+  $stmt->bindParam(':age', $age);
+  $stmt->execute();
 ?>
 
 
