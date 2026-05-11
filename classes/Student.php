ddebb69deb653266b2ef0d5bf279e9b49690a4d8
Sazzad Hossain <sazzadhossainmit@gmail.com>
2026-05-11 17:35:53 +0600

    CRUD with PDO - Update data

--- classes/Student.php
+++ classes/Student.php
@@ -26,7 +26,23 @@ include "DB.php";
       return $stmt->execute();
     }
 
+    public function update($id){
+      $sql = "UPDATE $this->table SET name=:name, dep=:dep, age=:age WHERE id=:id";
+      $stmt = DB::prepare($sql);
+      $stmt->bindParam(':id', $id);
+      $stmt->bindParam(':name', $this->name);
+      $stmt->bindParam(':dep', $this->dep);
+      $stmt->bindParam(':age', $this->age);
+      return $stmt->execute();
+    }
 
+    public function readById($id){
+      $sql = "SELECT * FROM $this->table WHERE id=:id";
+      $stmt = DB::prepare($sql);
+      $stmt->bindParam(':id', $id);
+      $stmt->execute();
+      return $stmt->fetch();
+    }
 
     public function readAll(){
       $sql = "SELECT * FROM $this->table";
--- index.php
+++ index.php
@@ -10,13 +10,81 @@
 
 
 
-
 <div class="content">
 <section class="subject">
 <p>CRUD with PDO - Create Data <span style="float:right"><a href="index.php">Create New</a></span><p>
 </section>
 
 <section class="mainleft">
+
+<?php 
+  $student = new Student();
+  if (isset($_POST['create'])){
+    $name = $_POST['name'];
+    $dep = $_POST['dep'];
+    $age = $_POST['age'];
+
+    $student->setName($name);
+    $student->setDep($dep);
+    $student->setAge($age);
+
+    if($student->insert()){
+      echo "<span style='color:green; font-weight:bold;'>Data added successfully...</span>";
+    }
+  }
+
+  if (isset($_POST['update'])){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $dep = $_POST['dep'];
+    $age = $_POST['age'];
+
+    $student->setName($name);
+    $student->setDep($dep);
+    $student->setAge($age);
+
+    if($student->update($id)){
+      echo "<span style='color:green; font-weight:bold;'>Data updated successfully...</span>";
+    }
+  }
+
+?>
+
+<?php 
+  if(isset($_GET['action']) && $_GET['action']=='update'){
+    $id = (int)$_GET['id'];
+    $result = $student->readById($id);
+?>
+
+<form action="" method="post">
+<table>
+  <input type="hidden" name="id" value="<?php echo $result['id']; ?>" />
+    <tr>
+        <td>Name: </td>
+        <td><input type="text" name="name" value="<?php echo $result['name']; ?>" required="1"/></td>    
+    </tr>
+
+    <tr>
+      <td>Department: </td>
+        <td><input type="text" name="dep" value="<?php echo $result['dep']; ?>" required="1"/></td>
+    </tr>
+
+    <tr>
+      <td>Age: </td>
+        <td><input type="text" name="age" value="<?php echo $result['age']; ?>" required="1"/></td>
+    </tr>
+    <tr>
+      <td></td>
+        <td>
+        <input type="submit" name="update" value="Update"/>
+        <input type="reset" value="Clear"/>
+        </td>
+    </tr>
+  </table>
+</form>
+
+<?php } else { ?>
+
 <form action="" method="post">
 <table>
     <tr>
@@ -43,22 +111,7 @@
   </table>
 </form>
 
-<?php 
-  $student = new Student();
-  if (isset($_POST['create'])){
-    $name = $_POST['name'];
-    $dep = $_POST['dep'];
-    $age = $_POST['age'];
-
-    $student->setName($name);
-    $student->setDep($dep);
-    $student->setAge($age);
-
-    if($student->insert()){
-      echo "<span style='font-weight: bold; color: green; margin-top: 20px;'> Data added succesfully...</span>";
-    }
-  }
-?>
+<?php } ?>
 
 </section>
 
@@ -86,7 +139,7 @@
         <td><?php echo $value['dep']; ?></td>
         <td><?php echo $value['age']; ?></td>
         <td>
-        <a href="">Edit</a> ||
+        <?php echo "<a href='index.php?action=update&id=".$value['id']."'>Edit</a>"; ?> ||
         <a href="">Delete</a>
         </td>
     </tr>
